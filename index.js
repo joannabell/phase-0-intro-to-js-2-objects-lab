5e5f7f0ecf176a29f884349d0c50c730f1c91dbf
Joanna Bell <jjbdigital@gmail.com>
2022-07-15 13:45:19 -0500

    commit tree d147efedb80c1e11dcc65d2c456ad34678b32327

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "John",
+    streetAddress: "123 Smith St, Jonestown, IL, 60540",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+   employee[key] = value;
+   return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newnewEmployee = {...employee};
+    delete newnewEmployee[key];
+    console.log(newnewEmployee);
+    return newnewEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
